59dcc5a06f99b5ef8091e872794d5b21e8c33695
newick <erick@entre-quote.com>
2017-11-28 13:46:35 +0100

    beginning of theme

--- cache/compiled/files/04a7cb049c8ea25f0966afdd63fc22ad.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/dev/Boulot/Mathieu Roy/ISCRA/iscra/user/config/media.yaml',
-    'modified' => 1510054900,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/e183136595532808bec33bed7de11f6c.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/dev/Boulot/Mathieu Roy/ISCRA/iscra/user/config/security.yaml',
-    'modified' => 1510054900,
-    'data' => [
-        'salt' => 'm4CLpNbhDMftvu'
-    ]
-];
